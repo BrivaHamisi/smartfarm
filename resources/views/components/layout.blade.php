636a041388b8ac2d62caddd1170fb2a4b4963cbe
BrivaHamisi <brivahamisi@gmail.com>
2025-01-22 11:25:30 +0300

    Fixed the header

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Business Software Solution</title>
+        <title>Smart Farm</title>
         <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Fonts -->
